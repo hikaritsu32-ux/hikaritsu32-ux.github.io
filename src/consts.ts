c513ab108ecc6e471a64fabe90dacff6c8579bd7
hikaritsu32-ux <hikaritsu32@gmail.com>
2026-06-23 00:05:13 +0800

    Change site title to 'Hikaritsu' and update description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Hikaritsu';
+export const SITE_DESCRIPTION = '“你只需要看着我就好，任何其他的风景...我都想把它们从你的视野里全部擦掉。”';
